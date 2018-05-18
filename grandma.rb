1aa864aab6b572be61ee94d98f246ea2928f96aa
Allegra <aukseallegra@gmail.com>
2018-05-18 15:37:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,8 +12,12 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
  def speak_to_grandma(string)
-   if string == string.upcase
-  return "HUH?! SPEAK UP, SONNY!"
+  if string != string.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
+  else 
+  
   
   
   
